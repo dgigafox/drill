d58517b8e630803ed0b1693cd1d9ac4a14ded2e5
Darren Gegantino <darren.gegantino@martide.com>
2022-05-04 22:29:05 +0800

    Add github workflow

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Drill.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -37,6 +38,13 @@ defmodule Drill.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
   end
 end
