270350ece4bd0f13774e69798f1cf26e6ac830ac
Darren Gegantino <darren.gegantino@martide.com>
2022-05-07 17:29:40 +0800

    Do not run seeds.exs in mix ecto.reset

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Drill.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
