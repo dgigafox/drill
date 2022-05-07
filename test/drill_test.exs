b2e217d54e0be227c27abf4f6ae32d73ce18a9eb
Darren Gegantino <darren.gegantino@martide.com>
2022-05-07 16:34:36 +0800

    Fix drill test

--- test/drill_test.exs
+++ test/drill_test.exs
@@ -2,7 +2,36 @@ defmodule DrillTest do
   use ExUnit.Case
   doctest Drill
 
-  test "greets the world" do
-    assert Drill.hello() == :world
+  contents =
+    quote do
+      use Drill, source: MySource, key: :my_key
+
+      def run(_), do: []
+    end
+
+  Module.create(Demo, contents, Macro.Env.location(__ENV__))
+
+  test "definitions" do
+    assert Demo.__info__(:functions) == [
+             autogenerate: 0,
+             constraints: 0,
+             context_key: 0,
+             deps: 0,
+             run: 1,
+             schema: 0
+           ]
+  end
+
+  test "context_key/0 returns opts `:key`" do
+    assert Demo.context_key() == :my_key
+  end
+
+  test "schema/0 returns opts `:source`" do
+    assert Demo.schema() == MySource
+  end
+
+  test "deps/0 and constraints/0 returns empty list by default" do
+    assert Demo.constraints() == []
+    assert Demo.deps() == []
   end
 end
